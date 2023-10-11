fe0de1054bc9c74e4c3821bc0a323b11bae77e21
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-10-11 17:14:21 +0400

    Add function for parse route response

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,8 @@
-from typing import Dict
+from typing import Dict, List
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pytz
+import re
 
 
 tz = pytz.timezone('Europe/Samara')
@@ -29,3 +30,14 @@ def get_current_hours():
 
 def get_current_minutes():
     return datetime.now(tz).strftime('%M')
+
+
+def get_route_values(text: str) -> List[List[str]]:
+    soup = BeautifulSoup(text, 'html.parser')
+    trs = soup.find_all('tr')
+    
+    values = list()
+    for tr in trs:
+        tds = tr.find_all('td')
+        values.append([re.sub(r'\s+', ' ', td.text.strip()) for td in tds])
+    return values[1:]
